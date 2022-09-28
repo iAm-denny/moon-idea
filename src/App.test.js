1386dc16ec8c9c8569c46d1894f7b96f6ff3def5
MLP <minlapyae8@gmail.com>
2022-09-28 18:29:58 +0630

    setting up the indexdb

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import store from './redux/store';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+  );
+  const linkElement = screen.getByText('Enhance');
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/utils/IndexDB/features.js
@@ -0,0 +1,53 @@
+/* eslint-disable import/prefer-default-export */
+import DB from './database';
+
+const onRequestError = (e) => {
+  // eslint-disable-next-line no-console
+  console.log('Database Error [TERNAK]', e);
+};
+
+export const get = (callback) => {
+  const request = DB();
+
+  request.onerror = onRequestError;
+
+  request.onsuccess = (e) => {
+    const db = e.target.result;
+    const transaction = db.transaction(['shapes'], 'readonly');
+    const store = transaction.objectStore('shapes');
+    store.getAll().onsuccess = (ev) => {
+      callback(ev.target.result);
+    };
+  };
+};
+
+export const add = (data) => {
+  const request = DB();
+
+  request.onerror = onRequestError;
+
+  request.onsuccess = (e) => {
+    const db = e.target.result;
+    const transaction = db.transaction(['shapes'], 'readwrite');
+    const store = transaction.objectStore('shapes');
+    store.add({ id: data.id, ...data });
+  };
+};
+
+export const searchbyId = (searchVal, callback) => {
+  if (searchVal) {
+    const request = DB();
+
+    request.onerror = onRequestError;
+
+    request.onsuccess = async (e) => {
+      const db = e.target.result;
+      const transaction = db.transaction(['shapes'], 'readonly');
+
+      const store = transaction.objectStore('shapes');
+      store.get(searchVal).onsuccess = (ev) => callback(ev.target.result);
+      return undefined;
+    };
+  }
+  return undefined;
+};
